d440234948b99690943eed6aeaac4823386b6a91
peterdeme <snypox@gmail.com>
2024-03-12 17:44:18 +0100

    Commented out
    Add randompet
    
    Uncomment
    
    Signed-off-by: peterdeme <snypox@gmail.com>

--- main.tf
+++ main.tf
@@ -8,3 +8,14 @@ import {
   id = "arn:aws:ssm:eu-central-1:705096403113:parameter/peterdeme-test-toimport"
   to = aws_ssm_parameter.testparam
 }
+
+resource "random_pet" "pet2" {
+  length = 5
+}
+
+moved {
+  from = random_pet.pet1
+  to   = random_pet.pet2
+}
+
+// 1
