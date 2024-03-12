4a8f5d531df18cfcde6415cc7fd53431d3299961
peterdeme <snypox@gmail.com>
2024-03-12 17:29:53 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_ssm_parameter" "testparam" {
+  name  = "peterdeme-test-toimport"
+  type  = "String"
+  value = "text"
+}
+
+import {
+  id = "arn:aws:ssm:eu-central-1:705096403113:parameter/peterdeme-test-toimport"
+  to = aws_ssm_parameter.testparam
+}
